cb39fb479b51fc7ebde82a5f1d51bc3a3cc463af
warsland <57026973+warsland@users.noreply.github.com>
2020-10-24 15:32:45 +0800

    Add files via upload

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,16 @@
+﻿def dichotomy(a,b,accurate,f):
+    time=0;
+    print(str(time)+ " a= "+str(a)+ ", b= "+str(b));
+    while a<b:
+        mid = a + abs(b-a)/2.0;
+        if abs(b-a) < accurate:
+            return mid;
+        if f(mid)*f(b) < 0:
+            a = mid;
+        if f(a)*f(mid) < 0:
+            b=mid;
+        time+=1;
+        print(str(time)+ " a= "+str(a)+ ", b= "+str(b));
+
+s = dichotomy(1.0,1.5,1e-2,lambda x: x**3-x-1 );
+print("answer= "+str(s));
